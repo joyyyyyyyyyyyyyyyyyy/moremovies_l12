e3f0310042e7a2c49ee10725adee3a89435a76fd
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2023-08-08 18:27:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22022096/moremovies_l12/MainActivity.java
@@ -0,0 +1,114 @@
+package sg.edu.rp.c346.id22022096.moremovies_l12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etmovietitle, etgenre, etyear;
+    Spinner spinner;
+    Button btninsert, btnshowlist;
+    String rating;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etmovietitle = findViewById(R.id.etmovietitle);
+        etgenre = findViewById(R.id.etgenre);
+        etyear = findViewById(R.id.etyear);
+        spinner = findViewById(R.id.spinner);
+        btninsert = findViewById(R.id.btninsert);
+        btnshowlist = findViewById(R.id.btnshowlist);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                switch(position){
+                    case 0:
+
+                        rating = "G";
+                        break;
+                    case 1:
+                        rating = "PG";
+                        break;
+                    case 2:
+                        rating = "PG13";
+                        break;
+                    case 3:
+                        rating = "NC16";
+                        break;
+                    case 4:
+                        rating = "M18";
+                        break;
+                    case 5:
+                        rating = "R21";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        btninsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get user input
+                String title = etmovietitle.getText().toString();
+                String genre = etgenre.getText().toString();
+                String stryr = etyear.getText().toString().trim();
+                //int year = Integer.valueOf(stryr);
+
+                int year;
+                try {
+                    year = Integer.parseInt(stryr);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Invalid year format. Please enter a valid year.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                DBHelper dbh = new DBHelper(MainActivity.this);
+
+                if (title.length() == 0 || genre.length() == 0){
+                    Toast.makeText(MainActivity.this, "please fill in movie title and genre", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                long result = dbh.insertMovie(title, genre, year, rating);
+
+                if (result != -1) {
+                    Toast.makeText(MainActivity.this, "movie successfully inserted", Toast.LENGTH_LONG).show();
+                    etmovietitle.setText("");
+                    etgenre.setText("");
+                    etyear.setText("");
+                } else {
+                    Toast.makeText(MainActivity.this, "insertion failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnshowlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, showmovies.class);
+                startActivity(i);
+                Log.d("activity started", "activity successfully started");
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id22022096/moremovies_l12/editmovie.java
@@ -0,0 +1,160 @@
+package sg.edu.rp.c346.id22022096.moremovies_l12;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class editmovie extends AppCompatActivity {
+
+    EditText etmovietitle, etgenre, etyear;
+    Spinner spinner;
+    Button btnupdate, btndelete, btncancel;
+    TextView tvMovieID, tvID, tvMovietitle, tvGenre, tvYear, tvRating;
+    String rating;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editmovie);
+
+        etmovietitle = findViewById(R.id.etmovietitle);
+        etgenre = findViewById(R.id.etgenre);
+        etyear = findViewById(R.id.etyear);
+        spinner = findViewById(R.id.spinner);
+        btnupdate = findViewById(R.id.btnupdate);
+        btndelete = findViewById(R.id.btndelete);
+        btncancel = findViewById(R.id.btncancel);
+        tvMovieID = findViewById(R.id.tvMovieID);
+        tvID = findViewById(R.id.tvID);
+        tvMovietitle = findViewById(R.id.tvMovietitle);
+        tvGenre = findViewById(R.id.tvGenre);
+        tvYear = findViewById(R.id.tvYear);
+        tvRating = findViewById(R.id.tvRating);
+
+        Intent intent = getIntent();
+        Movies data = (Movies) intent.getSerializableExtra("data");
+
+        tvID.setText(String.valueOf(data.getId()));
+        etmovietitle.setText(data.getTitle());
+        etgenre.setText(data.getGenre());
+        etyear.setText(String.valueOf(data.getYear()));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        rating = "G";
+                        break;
+                    case 1:
+                        rating = "PG";
+                        break;
+                    case 2:
+                        rating = "PG13";
+                        break;
+                    case 3:
+                        rating = "NC16";
+                        break;
+                    case 4:
+                        rating = "M18";
+                        break;
+                    case 5:
+                        rating = "R21";
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        btnupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper db = new DBHelper(editmovie.this);
+                data.setTitle(etmovietitle.getText().toString());
+                data.setGenre(etgenre.getText().toString());
+                data.setYear(Integer.parseInt(etyear.getText().toString()));
+                data.setRating(rating);
+                int result = db.updateMovie(data);
+                if (result>0){
+                    Toast.makeText(editmovie.this, "movie has been updated", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent();
+                    setResult(RESULT_OK);
+                    finish();
+                } else {
+                    Toast.makeText(editmovie.this, "failed to update movie", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(editmovie.this);
+                myBuilder.setTitle("Warning!");
+                myBuilder.setMessage("Are you sure you want to delete the movie " + data.getTitle()+"?");
+                myBuilder.setCancelable(true);
+
+                myBuilder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        DBHelper dbh = new DBHelper(editmovie.this);
+                        dbh.deleteMovie(data.getId());
+                        finish();
+                    }
+                });
+                myBuilder.setNeutralButton("Cancel", null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+                //DBHelper db = new DBHelper(editmovie.this);
+                //int result = db.deleteMovie(data.getId());
+                //if (result>0){
+                    //Toast.makeText(editmovie.this, "movie has been deleted", Toast.LENGTH_SHORT).show();
+                    //Intent i = new Intent();
+                    //setResult(RESULT_OK);
+                    //finish();
+                //} else {
+                    //Toast.makeText(editmovie.this, "failed to delete movie", Toast.LENGTH_SHORT).show();
+                //}
+            }
+        });
+
+        btncancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(editmovie.this);
+                myBuilder.setTitle("Warning!");
+                myBuilder.setMessage("Are you sure you want to discard the changes?");
+                myBuilder.setCancelable(true);
+
+                myBuilder.setPositiveButton("Discard", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+                myBuilder.setNeutralButton("Cancel", null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
+
+    }
+}
